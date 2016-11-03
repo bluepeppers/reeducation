a3e60aea22021a8a7620ff0fe74f47d19f4b9f4f
Laurie Clark-Michalek <laurie@qubit.com>
2016-11-03 18:01:00 +0000

    Bubble

---
+++ bubble/main.go
@@ -0,0 +1,21 @@
+package main
+
+func SortTwo(array [2]int) [2]int {
+	return array
+}
+
+func SortThree(array [3]int) [3]int {
+	return array
+}
+
+func MoveFirstElementUntilAllItemsBeforeItAreSmaller(slice []int) []int {
+	return slice
+}
+
+func KeepMovingTheFirstElementUntilTheSliceIsSorted(slice []int) []int {
+	return slice
+}
+
+func BubbleSort(slice []int) []int {
+	return slice
+}
---
+++ bubble/main_test.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortTwo(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input [2]int
+		wants [2]int
+	}{
+		{
+			[2]int{0, 0},
+			[2]int{0, 0},
+		},
+		{
+			[2]int{1, 2},
+			[2]int{1, 2},
+		},
+		{
+			[2]int{2, 1},
+			[2]int{1, 2},
+		},
+		{
+			[2]int{13, 1},
+			[2]int{1, 13},
+		},
+		{
+			[2]int{-13, 1},
+			[2]int{-13, 1},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			got := SortTwo(c.input)
+			if !reflect.DeepEqual(got, c.wants) {
+				t.Fatalf("Difference in expected result\nGot: %v\nExpected: %v\n", got, c.wants)
+			}
+		})
+	}
+}
+
+func TestSortThree(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input [3]int
+		wants [3]int
+	}{
+		{
+			[3]int{0, 0, 0},
+			[3]int{0, 0, 0},
+		},
+		{
+			[3]int{1, 3, 0},
+			[3]int{0, 1, 3},
+		},
+		{
+			[3]int{3, 1, 0},
+			[3]int{0, 1, 3},
+		},
+		{
+			[3]int{13, 1, 0},
+			[3]int{0, 1, 13},
+		},
+		{
+			[3]int{-13, 1, 30},
+			[3]int{-13, 1, 30},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			got := SortThree(c.input)
+			if !reflect.DeepEqual(got, c.wants) {
+				t.Fatalf("Difference in expected result\nGot: %v\nExpected: %v\n", got, c.wants)
+			}
+		})
+	}
+}
+
+func TestMoveFirstElementUntilAllItemsBeforeItAreSmaller(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input []int
+		wants []int
+	}{
+		{
+			[]int{0, 0, 0},
+			[]int{0, 0, 0},
+		},
+		{
+			[]int{1, 3, 0},
+			[]int{1, 3, 0},
+		},
+		{
+			[]int{3, 1, 0},
+			[]int{1, 0, 3},
+		},
+		{
+			[]int{13, 1, 0},
+			[]int{1, 0, 13},
+		},
+		{
+			[]int{-13, 1, 30},
+			[]int{-13, 1, 30},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			got := MoveFirstElementUntilAllItemsBeforeItAreSmaller(c.input)
+			if !reflect.DeepEqual(got, c.wants) {
+				t.Fatalf("Difference in expected result\nGot: %v\nExpected: %v\n", got, c.wants)
+			}
+		})
+	}
+}
+
+func TestKeepMovingTheFirstElementUntilTheSliceIsSorted(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input []int
+		wants []int
+	}{
+		{
+			[]int{0, 0, 0},
+			[]int{0, 0, 0},
+		},
+		{
+			[]int{1, 3, 0},
+			[]int{0, 1, 3},
+		},
+		{
+			[]int{3, 1, 0},
+			[]int{0, 1, 3},
+		},
+		{
+			[]int{13, 1, 0},
+			[]int{0, 1, 13},
+		},
+		{
+			[]int{-13, 1, 30},
+			[]int{-13, 1, 30},
+		},
+		{
+			[]int{-13, 20, 30, 0, 9, 10000},
+			[]int{-13, 0, 9, 20, 30, 10000},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			got := KeepMovingTheFirstElementUntilTheSliceIsSorted(c.input)
+			if !reflect.DeepEqual(got, c.wants) {
+				t.Fatalf("Difference in expected result\nGot: %v\nExpected: %v\n", got, c.wants)
+			}
+		})
+	}
+}
+
+func TestBubbleSort(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input []int
+		wants []int
+	}{
+		{
+			[]int{0, 0, 0},
+			[]int{0, 0, 0},
+		},
+		{
+			[]int{1, 3, 0, 10, 39, 2},
+			[]int{0, 1, 2, 3, 10, 39},
+		},
+		{
+			[]int{1, 2, -1},
+			[]int{-1, 1, 2},
+		},
+		{
+			[]int{13, 1, 0},
+			[]int{0, 1, 13},
+		},
+		{
+			[]int{-13, 1, 30},
+			[]int{-13, 1, 30},
+		},
+		{
+			[]int{-13, 20, 30, 0, 9, 10000, -1, 0},
+			[]int{-13, -1, 0, 0, 9, 20, 30, 10000},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			got := BubbleSort(c.input)
+			if !reflect.DeepEqual(got, c.wants) {
+				t.Fatalf("Difference in expected result\nGot: %v\nExpected: %v\n", got, c.wants)
+			}
+		})
+	}
+}
