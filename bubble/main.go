61080a37f2ef4df4a1df7722648c6ec0a2c077eb
Laurie Clark-Michalek <laurie@qubit.com>
2016-11-03 18:14:44 +0000

    Docs i guess

--- bubble/main.go
+++ bubble/main.go
@@ -1,17 +1,30 @@
 package main
 
+import (
+	"fmt"
+)
+
+// I mean, this is a sort, if there are only two items, right? Order here (and everywhere else) means that the earlier items are smaller (ascending?)
 func SwapFirstTwoElementsIfOutOfOrder(slice []int) []int {
 	return slice
 }
 
+// Check the test cases if this doesnt make sense.
+//
+// [x, a, b, c, d] -> [a, b, c, x, d] (if d is the only item that is bigger than X)
 func MoveFirstElementUntilAllItemsBeforeItAreSmaller(slice []int) []int {
 	return slice
 }
 
+// Sorted as in [0, 1, 2, 3]
 func KeepMovingTheFirstElementUntilTheSliceIsSorted(slice []int) []int {
 	return slice
 }
 
 func BubbleSort(slice []int) []int {
 	return slice
+}
+
+func main() {
+	fmt.Println("Test stuff:", SwapFirstTwoElementsIfOutOfOrder([]int{0, 1}))
 }
